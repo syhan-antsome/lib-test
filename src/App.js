a10a976c1dfdccbb07bba845192afe39848afba2
syhan <syhan@antsome.com>
2023-09-26 16:19:27 +0900

    XTasRelGraph 컴포넌트 파라미터 테스트

--- src/App.js
+++ src/App.js
@@ -10,23 +10,21 @@ function App() {
       <br /><br />
 
       <DetectRelGraph nodeData={{
-        mainIp: "123.112.33.55",
+        mainIp: "123.112.33.57",
         detect: {
           inside: [{
             nodeIp: "211.11.22.33",
-            lineIp: "11.55.23.98",
-            inout: "IN"
+            inout: "OUT"
           }],
           outside: [{
             nodeIp: "211.11.22.33",
-            lineIp: "11.55.23.98",
             inout: "IN"
           }]
         },
         result: {
           inside: [{
-            nodeIp: "111.11.22.11",
-            inout: "OUT"
+            nodeIp: "111.11.22.999",
+            inout: "IN"
           }, {
             nodeIp: "111.11.22.22",
             inout: "OUT"
@@ -54,12 +52,86 @@ function App() {
             inout: "IN"
           }]
         }
-      }}/>
+      }} />
 
       <br /><br />
 
       <XTasRelGraph nodeData={{
-      }}/>
+        "ntas": {
+          "name": "DNS covert channel"
+        },
+        "process": {
+          "t1": [{
+            "name": "dfwf"
+          },
+          {
+            "name": "rhfdg"
+          }
+          ],
+          "t2": [{
+            "name": "rtdfag"
+          }],
+          "t3": [{
+            "name": "nrefgerfg"
+          }],
+          "t4": [{
+            "name": "nrefgerfg"
+          }],
+          "t5": [{
+            "name": "nrefgerfg"
+          },{
+            "name": "nrefgerfg"
+          },{
+            "name": "nrefgerfg"
+          },{
+            "name": "nrefgerfg"
+          }]
+        },
+        "file": {
+          "t1": [],
+          "t2": [{
+            "name": "y5h5rtf"
+          },{
+            "name": "y5h5rtf"
+          }],
+          "t3": [],
+          "t4": [{
+            "name": "y5h5rtf"
+          }],
+          "t5": []
+        },
+        "registry": {
+          "t1": [{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          }],
+          "t2": [],
+          "t3": [{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          },{
+            "name": "rth5yhaegr"
+          }],
+          "t4": [{
+            "name": "rth5yhaegr"
+          }],
+          "t5": [{
+            "name": "rth5yhaegr"
+          }]
+        }
+      }} />
 
 
     </div>
